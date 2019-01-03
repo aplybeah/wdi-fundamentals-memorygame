7cefc8f64828d1f51a55f2f30b985ae595af1548
Alsia Plybeah <Alsia1@Alsias-MBP.fios-router.home>
2019-01-03 17:40:44 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,18 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " "+ cardFour);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+cardsInPlay.length;
+    if (cardsInPlay.length === 2 ) {
+        console.log("Let's Hope they match!");
+        if (cardsInPlay[0]=== cardsInPlay[1]) {
+            alert("You found a match!");
+
+        } else if (cardsInPlay[0]!== cardsInPlay[1]) {
+            alert("Sorry, try again...");
+        }
+    }
